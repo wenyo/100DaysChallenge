f980e5c4b29e98a0bd35dc2c597d0d0e91989d07
wen yo <wenda897436@gmail.com>
2021-03-25 22:46:56 +0800

    :construction: Intro componet init

--- 002day/src/component/Intro.js
+++ 002day/src/component/Intro.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Context from '../context/Context';
 
 const Intro = () => {
-    return <div>Intro</div>;
+    const { sNowYear } = useContext(Context);
+
+    return (
+        <div className="Intro">
+            <h1>{sNowYear} Mode Calendar</h1>
+            <ul>
+                <li>choese mode first</li>
+                <li>choese date and mark it</li>
+            </ul>
+        </div>
+    );
 };
 
 export default Intro;
---
+++ 002day/src/context/Context.js
@@ -0,0 +1,10 @@
+import React, { createContext } from 'react';
+
+const sNowYear = new Date().getFullYear();
+
+const context = createContext({
+    sNowYear,
+});
+
+export default context;
+export const { Provider, Consumer } = context;
