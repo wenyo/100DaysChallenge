85af909f1ef67e2611c3c2ce5871983e8b68091f
wen yo <wenda897436@gmail.com>
2021-03-25 22:32:30 +0800

    :pencil2: fix typo

---
+++ 002day/src/component/Calendar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Calendar extends React.Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return <>Calendar</>;
+    }
+}
+
+export default Calendar;
